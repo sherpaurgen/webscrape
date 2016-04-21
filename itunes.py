58c37d452abc39e43ab0ee163fc1cd79e4f3ded1
urgen sherpa <norbuurgen@gmail.com>
2016-04-22 01:13:38 +0545

    change onitunes.py for pulling div text

--- itunes.py
+++ itunes.py
@@ -1,3 +1,6 @@
+from lxml import html
+import requests
+
 class AppCrawler:
     
     def __init__(self,starting_url,depth):
@@ -6,22 +9,30 @@ class AppCrawler:
         self.apps=[]
 
     def crawl(self):
+    	self.get_app_from_link(self.starting_url)
     	return 
 
     def get_app_from_link(self,link):
-    	return
+    	start_page=requests.get(link)
+    	#print start_page.text
+    	tree = html.fromstring(start_page.text)
+    	name = tree.xpath('//div[@class="id-app-title"]/text()')[0]
+    	print name
+    	return 
+
 class App:
 	def __init__(self, name,developer,price,links):
 		self.name=name
 		self.developer=developer
 		self.price = price
 		self.links=links
+
 	def __str__(self):
 		return("Name"+self.name.encode('UTF-8')+
 		"\nDeveloper: " + self.developer.encode('UTF-8')+
 		"\nPrice: " + self.price.encode('UTF-8')+"\n")
 
-crawler = AppCrawler()
+crawler = AppCrawler('https://play.google.com/store/apps/details?id=com.hecorat.packagedisabler',0)
 crawler.crawl()
 for app in crawler.apps:
 	print app
